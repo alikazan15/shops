af37b47882db5acec40f6afe4d1d8c051432983b
syscomlb <syscomlb@gmail.com>
2025-09-11 00:42:46 +0300

    view brands

--- database/migrations/2025_09_10_204452_create_brands_table.php
+++ database/migrations/2025_09_10_204452_create_brands_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('mobile')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('utype')->default('USR')->comment('ADM for Admin and USR for user or Customer');
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('brands');
     }
 };
