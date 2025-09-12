c17d5c27c5ce0b91d20b8773397c53fc50b58767
syscomlb <syscomlb@gmail.com>
2025-09-13 01:00:39 +0300

    update brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Brand;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+
+
 
 class AdminController extends Controller
 {
@@ -12,9 +17,39 @@ class AdminController extends Controller
         return view('admin.index');
     }
 
-    public function brands(){
-        $brands=Brand::orderBy('id','DESC')->paginate(10);
-        return view('admin.brands',compact('brands'));
+    public function brands()
+    {
+        $brands = Brand::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.brands', compact('brands'));
+    }
 
+    public function add_brand()
+    {
+        return view('admin.brand-add');
+    }
+
+    public function brand_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $image = $request->file('image');
+        $file_extention = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extention;
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been added succesfully!');
+    }
+
+    public function GenerateBrandThumbailsImage($image,$imageName)
+    {
+        $destinationPath=public_path('uploads/brands');
+        $image=Image::read($image->path);
     }
 }
